3cc74cee4f35bdabffedb01655fbe3c4600c4e7c
emmakuo1019 <emma20041019@gmail.com>
2024-12-11 23:15:56 +0800

    測標滑動效果
    待增：滑鼠懸停效果

--- home.js
+++ home.js
@@ -1,16 +1,30 @@
-$(document).ready(function(){
-
-    $('.fish').click(function(){
-        $('.object').slideToggle(500);
-    });
-
-    $('.angry').click(function(){
-        $('.object').slideToggle(500);
-    });
-
-});
-
 $(document).ready(function () {
+
+    $( "#top" ).on( "click", function() {
+        $("html,body").animate(
+            {
+                scrollTop:0
+            })
+    } );
+
+    $( "#art" ).on( "click", function() {
+        $("#art-area").get(0).scrollIntoView(
+            {behavior:"smooth"}
+        );
+    } );
+        
+    $( "#picture" ).on( "click", function() {
+        $("#picture-area").get(0).scrollIntoView(
+            {behavior: "smooth"}
+        );
+    } );
+
+    $( "#game" ).on( "click", function() {
+        $("#game-area").get(0).scrollIntoView(
+            {behavior: "smooth"}
+        );
+    } );
+    
     const $track = $('.carousel-track');
     const $carousel = $('.carousel');
     const speed = 50; // 控制滾動速度，數字越小越快
@@ -22,7 +36,7 @@ $(document).ready(function () {
 
     function startScrolling() {
         scrollAmount -= 1; // 每次向左滾動 1 像素
-        if (Math.abs(scrollAmount) >= $track.width() / 2) {
+        if (Math.abs(scrollAmount) >= $track.width() / 1) {
             scrollAmount = 0; // 當滾動超過一半，重置位置
         }
         $track.css('transform', `translateX(${scrollAmount}px)`);
@@ -33,6 +47,14 @@ $(document).ready(function () {
 });
 
 /*$(document).ready(function () {
+    $('.fish').click(function(){
+        $('.object').slideToggle(500);
+        });
+
+        $('.angry').click(function(){
+        $('.object').slideToggle(500);
+    });
+    
     let currentIndex = 0; // 記錄當前顯示圖片的索引
     const artworks = $('.slideshow .artwork'); // 取得所有的 artwork 圖片
     $(artworks[currentIndex]).show(); // 初始顯示第一張圖片
