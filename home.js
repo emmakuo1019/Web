5861b2f7811a6b0f8f39bf096f377297f2fd1507
emmakuo1019 <emma20041019@gmail.com>
2024-12-12 16:31:20 +0800

    增加偵測進入視窗執行動畫

--- home.js
+++ home.js
@@ -44,6 +44,25 @@ $(document).ready(function () {
     }
 
     startScrolling();
+
+    const elements = document.querySelectorAll(".line")
+    function observeHandler(entries) {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                // 當元素進入視窗時觸發動畫
+                var $div = $(entry.target); // 使用 jQuery 選取目標元素
+                $div.animate({ width: '100%', opacity: '0.4' }, "slow");
+                // 停止觀察該元素，避免重複動畫
+                observer.unobserve(entry.target);
+            }
+        })
+    }
+    const observer = new IntersectionObserver(observeHandler);
+
+    // 開始觀察指定的元素
+    elements.forEach(element =>{
+        observer.observe(element);
+    })
 });
 
 /*$(document).ready(function () {
