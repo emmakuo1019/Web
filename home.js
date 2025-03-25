189e983ec8888390f94e76d9d154976f6aaec27c
emmakuo1019 <emma20041019@gmail.com>
2025-03-25 23:15:04 +0800

    程式碼整理
    程式碼整理、以及置入作品
    待辦：加作品介紹頁面

--- home.js
+++ home.js
@@ -1,54 +1,33 @@
 $(document).ready(function () {
 
-    $( "#top" ).on( "click", function() {
+    $( ".top" ).on( "click", function() {
         $("html,body").animate(
             {
                 scrollTop:0
             })
     });
 
-    $( "#art" ).on( "click", function() {
-        $("#art-area").get(0).scrollIntoView(
+    $( "#introduction" ).on( "click", function() {
+        $("#introduction-area").get(0).scrollIntoView(
             {behavior:"smooth"}
         );
     } );
         
-    $( "#picture" ).on( "click", function() {
-        $("#picture-area").get(0).scrollIntoView(
+    $( "#design" ).on( "click", function() {
+        $("#design-area").get(0).scrollIntoView(
             {behavior: "smooth"}
         );
     } );
 
-    $( "#game" ).on( "click", function() {
-        $("#game-area").get(0).scrollIntoView(
+    $( "#website" ).on( "click", function() {
+        $("#website-area").get(0).scrollIntoView(
             {behavior: "smooth"}
         );
     } );
 
-    $(".contact").click(function(){
-        $('.object').slideToggle(500);
-    });
-
-    const $track = $('.carousel-track');
-    const $carousel = $('.carousel');
-    const speed = 50; // 控制滾動速度，數字越小越快
-
-    // 複製內容以實現無縫滾動
-    $track.append($track.html());
-    $track.append($track.html());
-
-    let scrollAmount = 0;
-
-    function startScrolling() {
-        scrollAmount -= 1; // 每次向左滾動 1 像素
-        if (Math.abs(scrollAmount) >= $track.width() / 1) {
-            scrollAmount =0;
-        }
-        $track.css('transform', `translateX(${scrollAmount}px)`);
-        requestAnimationFrame(startScrolling); // 使用更流暢的動畫更新
-    }
-
-    startScrolling();
+    /*$(".contact").click(function(){
+        $('.linkbox').slideToggle(500);
+    });*/
 
     const elements = document.querySelectorAll(".line")
     function observeHandler(entries) {
@@ -82,21 +61,3 @@ $(document).ready(function () {
             })
     });
 });
-
-/*
-    
-    let currentIndex = 0; // 記錄當前顯示圖片的索引
-    const artworks = $('.slideshow .artwork'); // 取得所有的 artwork 圖片
-    $(artworks[currentIndex]).show(); // 初始顯示第一張圖片
-
-    $('.slideshow').click(function () {
-        $(artworks[currentIndex]).hide(); // 隱藏當前圖片
-        currentIndex = (currentIndex + 1) % artworks.length; // 更新索引到下一張（若超出範圍則回到第一張）
-        $(artworks[currentIndex]).fadeIn(); // 以淡入效果顯示下一張圖片
-    });
-
-    $('#target').on('input', function() {
-        var inputValue = $(this).val();
-        $('#display').text(inputValue);
-    });
-    */
